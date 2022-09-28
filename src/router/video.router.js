177bf44b340e145b403af4d18113475d1704dc2c
LikeK423 <88269600+LikeK423@users.noreply.github.com>
2022-09-28 19:18:14 +0800

    更新 video.router.js

--- src/router/video.router.js
+++ src/router/video.router.js
@@ -6,7 +6,7 @@ const {
 	biliVideoRecommend
 } = require('../controller/video.controller');
 
-console.log("文件 video.router.js OK");
+
 videoRouter.get('/bili/video',biliVideoRecommend);// 获取BiliBili首页推荐视频信息
 
 module.exports = videoRouter;
