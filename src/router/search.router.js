778601e06f01dc9609299f0a24f37f38d4f35f56
LikeK423 <88269600+LikeK423@users.noreply.github.com>
2022-10-04 13:19:30 +0800

    更新 search.router.js

--- src/router/search.router.js
+++ src/router/search.router.js
@@ -10,8 +10,8 @@ const {
 
 const searchRouter = new Router({prefix:'/api'});
 
-searchRouter.get('/google/search',googleSearch);// 谷歌搜索
-searchRouter.get('/bing/search',bingSearch);// 必应搜索
+searchRouter.get('/google/search',googleSearch);// 谷歌搜索 - 无效
+searchRouter.get('/bing/search',bingSearch);// 必应搜索 - 无效
 searchRouter.get('/wangyi/search',wangyiSearch);// 网易云搜索
 searchRouter.get('/bili/search',biliSearch); // BiliBili搜索
 
